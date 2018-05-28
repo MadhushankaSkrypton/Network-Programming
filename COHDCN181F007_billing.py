cbd52138bfd9b1d210e70e112190661b5b151397
MadhushankaSkrypton <39589700+MadhushankaSkrypton@users.noreply.github.com>
2018-05-28 23:34:10 +0530

    Add files via upload

---
+++ COHDCN181F007_billing.py
@@ -0,0 +1,33 @@
+product_box=[]
+price_box=[]
+for i in range(10000):
+    product=str(input("Name of Product "+str(i+1)+" :"))
+    if product=='':
+        break
+    while True:
+        try:
+            price=float(input("Product Price "+str(i+1)+" :"))
+            break
+        except ValueError:
+            print("Please Enter a Numeric Value")
+    
+    product_box.append(str(product))
+    price_box.append(float(price))
+total=sum(price_box)
+if total >5000 :
+    discount = 12
+elif total > 3000 :
+    discount = 10
+elif 3000>total >1000 :
+    discount = 5
+elif total <1000 :
+    discount = 0
+dis=total*discount/100.0
+net_price=total-dis
+text_file=open(" SavedData.txt" ,  "w")
+text_file.write(" Item Name: %s" % product_box )
+text_file.write(" Item Price: %s" % price_box )
+text_file.write(" Discount Precentage: %s" % discount )
+text_file.write(" Saved Price: %s" % dis )
+text_file.close()
+print("\n\nNet Price = Rs.",net_price)
